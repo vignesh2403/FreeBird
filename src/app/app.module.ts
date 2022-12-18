44055538003e18f8027fd0b5013e47063c6fbc9f
vignesh2403 <vigneshperiyasamy1999@gmail.com>
2022-12-18 09:15:04 -0800

    loginpage UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { LoginPageContainerComponent } from './login-page/login-page-container/login-page-container.component';
+import { UsersListComponent } from './login-page/users-list/users-list.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent],
+  declarations: [AppComponent, LoginPageContainerComponent, UsersListComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ src/app/login-page/login-page-container/login-page-container.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login-page-container',
+  templateUrl: './login-page-container.component.html',
+  styleUrls: ['./login-page-container.component.css']
+})
+export class LoginPageContainerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
